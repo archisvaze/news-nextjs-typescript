df785a000c9a9092f99884b38ca495d192189f0a
archis <vazearchis@gmail.com>
2022-10-20 15:05:59 +0530

    nav bar transparency

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,7 @@ const Navbar = () => {
   const router = useRouter();
   console.log(router.asPath)
   return (
-    <nav className='flex justify-center gap-8 px-8 items-center  bg-white dark:bg-slate-900 text-black dark:text-white p-4 w-full fixed z-20'>
-
+    <nav className='flex justify-center gap-4 px-4 items-center  bg-white/30 dark:bg-slate-900/30 text-black dark:text-white p-4 w-full z-20 flex-wrap fixed backdrop-blur-lg'>
 
 
       <Link href={"/"}><a className={`py-1 px-2 rounded-md ${router.asPath === "/" ? "  bg-slate-300 dark:bg-slate-600" : ""}`} rel="noopener noreferrer">World</a></Link>
@@ -22,9 +21,9 @@ const Navbar = () => {
 
       <Link href={"/sports"}><a className={`py-1 px-2 rounded-md ${router.asPath === "/sports" ? "  bg-slate-300 dark:bg-slate-600" : ""}`} rel="noopener noreferrer">Sports</a></Link>
 
-      <Link href={"/entertainment"}><a className={`py-1 px-2 rounded-md ${router.asPath === "/entertainment" ? "  bg-slate-300 dark:bg-slate-600" : ""}`} rel="noopener noreferrer">Entertainment</a></Link>
+      <Link href={"/entertainment"}><a className={`py-1 px-2 rounded-md ${router.asPath === "/entertainment" ? "  bg-slate-300 dark:bg-slate-600" : ""}`} rel="noopener noreferrer">TV</a></Link>
 
-      <Link href={"/miscellaneous"}><a className={`py-1 px-2 rounded-md ${router.asPath === "/miscellaneous" ? "  bg-slate-300 dark:bg-slate-600" : ""}`} rel="noopener noreferrer">Miscellaneous</a></Link>
+      <Link href={"/miscellaneous"}><a className={`py-1 px-2 rounded-md ${router.asPath === "/miscellaneous" ? "  bg-slate-300 dark:bg-slate-600" : ""}`} rel="noopener noreferrer">Misc</a></Link>
     </nav>
   )
 }
